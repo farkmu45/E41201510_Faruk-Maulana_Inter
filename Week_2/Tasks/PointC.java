dde376d63d2363243f68b769a7a980e66af904e5
Faruk <farukmicros@outlook.com>
2021-03-16 01:41:27 +0700

    Add files for point C & D

---
+++ Week_2/Tasks/PointC.java
@@ -0,0 +1,15 @@
+package Week_2.Tasks;
+
+public class PointC {
+    public static void main(String[] args) {
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number1;
+
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }
+}
