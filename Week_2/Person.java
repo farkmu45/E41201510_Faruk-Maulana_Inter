5ec1954f33b5ea307ebde2da061fcc5ceb7cc9f0
Faruk <farukmicros@outlook.com>
2021-03-16 05:52:18 +0700

    Add week 2 tasks

---
+++ Week_2/Person.java
@@ -0,0 +1,38 @@
+/**
+ * Person
+ */
+package Week_2;
+
+public class Person {
+
+    private String fName, lName, stuStatus;
+    private int stuId, age;
+
+    public Person(String fName, String lName, String stuStatus, int stuId) {
+        this.fName = fName;
+        this.lName = lName;
+        this.stuStatus = stuStatus;
+        this.stuId = stuId;
+    }
+
+    public Person(String fName, int age) {
+        this.fName = fName;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return fName;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("Lisa", "Palombo", "Active", 123456789);
+
+        System.out.println("Student Name    : " + person.fName + " " + person.lName);
+        System.out.println("Student ID      : " + person.stuId);
+        System.out.println("Student Status  : " + person.stuStatus);
+    }
+}
