6b5f7c44d6f5cfa92647e52452ab8c59f517512c
Faruk <farukmicros@outlook.com>
2021-03-29 11:05:50 +0700

    Comment unused variables

--- Week_2/Tasks/PointC.java
+++ Week_2/Tasks/PointC.java
@@ -2,10 +2,10 @@ package Week_2.Tasks;
 
 public class PointC {
     public static void main(String[] args) {
-        boolean gameOver = false;
-        int students = 50, classes = 3;
-        double sales_tax;
-        short number1;
+        // boolean gameOver = false;
+        // int students = 50, classes = 3;
+        // double sales_tax;
+        // short number1;
 
         // int 2beOrNot2be;
         // float price index;
