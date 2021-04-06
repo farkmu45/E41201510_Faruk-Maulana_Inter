2ffe8b2deb52ce8102f625cbec3550171bd57b63
Faruk <farukmicros@outlook.com>
2021-04-06 18:22:14 +0700

    Add week 5 tasks

---
+++ Week_5/Examples/Node.java
@@ -0,0 +1,13 @@
+package Week_5.Examples;
+
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void displayNode() {
+        System.out.print("{ " + id + ", " + data + " } ");
+    }
+
+}
---
+++ Week_5/Examples/Vertex.java
@@ -0,0 +1,11 @@
+package Week_5.Examples;
+
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+
+    public Vertex(char label) {
+        this.label = label;
+        this.wasVisited = false;
+    }
+}
